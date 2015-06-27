b1a9bafa9ad481e7db94b3642808acd1d9f829d9
Junzi Sun <junzi.sun@gmail.com>
2015-06-27 08:01:07 +0200

    complete machine learning with SVM

--- extraction.py
+++ extraction.py
@@ -1,4 +1,5 @@
 import os
+import time
 import uuid
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,9 +7,7 @@ import matplotlib.pyplot as plt
 from skimage import io
 from skimage import morphology
 from skimage import measure
-
-import pytesseract
-from PIL import Image
+from skimage import transform
 
 def check_letter(contour):
         xmax = max(contour[:, 1])
@@ -32,22 +31,23 @@ def save_contour_images(image, contours):
 
         if check_letter(contour):
             subimg =  image[ymin:ymax, xmin:xmax]
+            newimg = transform.resize(subimg, (40, 30))
             try:
                 uid = str(uuid.uuid1())
-                io.imsave( 'data/output/' + uid + '.jpg', subimg)
+                io.imsave( 'data/tmp/output/' + uid + '.jpg', newimg)
             except:
                 pass    
     return
 
 def process_image(imgfile):
     '''read image in grey mode'''
-    image = origimg = io.imread(imgfile, as_grey=True)
+    image = img0 = io.imread(imgfile, as_grey=True)
 
     '''pre-process image'''
     # keep only white part (letters are usually white)
-    image = morphology.white_tophat(image, morphology.disk(15))
+    image = img1 = morphology.white_tophat(image, morphology.disk(15))
     # convert to pure black-white
-    image = np.where(image > 0.9, 0.0, 1.0)
+    image = img2 = np.where(image > 0.9, 0.0, 1.0)
 
     '''Image segmentation'''
     contours = measure.find_contours(image, 0.8)
@@ -57,7 +57,7 @@ def process_image(imgfile):
 
     '''Display the image and plot all contours found'''
     fig, ax = plt.subplots()
-    ax.imshow(origimg, interpolation='nearest', cmap=plt.cm.gray)
+    ax.imshow(img0, interpolation='nearest', cmap=plt.cm.gray)
 
     # plot contours
     nletters = 0
@@ -70,6 +70,8 @@ def process_image(imgfile):
     plt.show()
 
 
-# for imgfile in os.listdir('data/memes/'):
-#   process_image('data/memes/'+imgfile)
-process_image('data/memes/duck-1.jpg')
+#process_image('data/memes/blb-1.jpg')
+
+for imgfile in os.listdir('data/memes/'):
+    print 'Processing image: '+imgfile
+    process_image('data/memes/'+imgfile)
---
+++ train.py
@@ -0,0 +1,81 @@
+import os
+import numpy as np
+from scipy import misc
+from time import time
+from matplotlib import pyplot as plt
+
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import RandomizedPCA
+from sklearn.svm import SVC
+
+
+def read_image(imgfile):
+    ''' read a char image, and convert into 1D list
+    '''
+    img_raw = misc.imread(imgfile)
+    img_1d_list = img_raw.reshape([1, -1])[0].tolist()
+    return img_1d_list
+
+
+####################################################################
+# Process data in the training dir, prepare it for learning
+imgdata = [] 
+imglabels = []
+
+for c in os.listdir('data/train'):
+    ldir = 'data/train/' + c
+    if os.path.isdir(ldir):
+        for f in os.listdir(ldir):
+            fpath = ldir + '/' + f
+            if f.endswith('.jpg'):
+                imgdata.append(read_image(fpath))
+                imglabels.append(c)
+
+# dataset and label in numpy array
+X = np.asarray(imgdata)
+y = np.asarray(imglabels)
+
+# split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+
+####################################################################
+# Apply PCA, reduce input dimensions space from 1200 to lower
+n_components = 200
+
+t0 = time()
+pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
+print("PCA done in %0.3fs" % (time() - t0))
+
+t0 = time()
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
+print("Data transform done in %0.3fs" % (time() - t0))
+
+
+####################################################################
+# Apply SVM classifier
+# Before apply SVC, use a search method to find the best params
+#   C - Penalty parameter C of the error term.
+#   gamma - Kernel coefficient
+t0 = time()
+param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
+clf = GridSearchCV(SVC(kernel='rbf', class_weight='auto'), param_grid)
+clf.fit(X_train_pca, y_train)
+print("Clf done in %0.3fs" % (time() - t0))
+print("Best estimator found by grid search:")
+print(clf.best_estimator_)
+
+
+####################################################################
+# Prediction and validation
+y_pred = clf.predict(X_test_pca)
+print(classification_report(y_test, y_pred))
+# print(confusion_matrix(y_test, y_pred))
+eigenltrs = pca.components_.reshape((200, 40, 30))
+plt.imshow(eigenltrs[1], cmap=plt.cm.gray)
+plt.show()
